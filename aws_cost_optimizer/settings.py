b9aca2a92f6e40d6c41b4829095fcee3b342a5da
the0therguy <nblues399@gmail.com>
2023-12-29 17:58:16 +0600

    custom exception added.

--- aws_cost_optimizer/settings.py
+++ aws_cost_optimizer/settings.py
@@ -56,7 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
+    # 'utils.custom_exception_handler.ExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'aws_cost_optimizer.urls'
